cde8e70f1c0350167bbe8664cc1f4a25742e7626
Atomanti007 <atomanti007@citromail.hu>
2021-06-07 14:46:24 +0200

    Update

--- src/action.js
+++ src/action.js
@@ -11,24 +11,7 @@ async function run() {
 
     console.log(core.getInput(`GITHUB_REPOSITORY`));
 
-    if ( typeof TENOR_TOKEN !== 'string' ) {
-        throw new Error('Invalid TENOR_TOKEN: did you forget to set it in your action config?');
-    }
 
-    if ( typeof GITHUB_TOKEN !== 'string' ) {
-        throw new Error('Invalid GITHUB_TOKEN: did you forget to set it in your action config?');
-    }
-
-    const randomPos = Math.round(Math.random() * 1000);
-    const url = `https://api.tenor.com/v1/search?q=${encodeURIComponent(searchTerm)}&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high`
-
-    console.log(`Searching Tenor: ${url} `)
-
-    const response = await fetch(`${url}&key=${TENOR_TOKEN}`);
-    const { results } = await response.json();
-    const gifUrl = results[0].media[0].tinygif.url;
-
-    console.log(`Found gif from Tenor: ${gifUrl}`);
 
     const { context = {} } = github;
     const { pull_request } = context.payload;
